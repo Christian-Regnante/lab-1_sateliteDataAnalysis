0d7117b57c3bf764e1ede5285db5e09d353f8b1c
Christian-Regnante <c.ngwino@alustudent.com>
2025-02-09 13:02:16 -0800

    Resolved some logic errors in satelite shell file.

--- satelite.sh
+++ satelite.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 students=satelite_records
-mkdir $students
-sort -t, -k3 -n -r raw_data/satelite_temperature_data.csv | uniq | head -10 > $students/highest_temp.csv
-grep Angola raw_data/satelite_temperature_data.csv | sort -t, -k4 -n -r | awk -F, '!seen[$4]' > $students/humidity_data_Angola.csv
+data=analyzed_data
+mkdir $data/$students
+sort -t, -k3 -n -r raw_data/satelite_temperature_data.csv | uniq | head -10 > $data/$students/highest_temp.csv
+grep Angola raw_data/satelite_temperature_data.csv | sort -t, -k4 -n -r | awk -F, '!seen[$4]' > $data/$students/humidity_data_Angola.csv
 
